11b375f5497e56f8178b6819d7c0d7140799dea2
arseniysij <50396694+arseniysij@users.noreply.github.com>
2019-08-14 14:29:05 +0500

    build

---
+++ dist/js/_custom.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var burger = document.querySelector(".menu-nav-toggler");
+var modalMenu = document.querySelector(".modal-menu");
+var modalClose = document.querySelector(".modal-nav-btn")
+	
+burger.addEventListener("click", function(e){
+	e.preventDefault();
+	modalMenu.classList.add("active");
+});
+
+modalClose.addEventListener("click", function(e){
+	e.preventDefault();
+	modalMenu.classList.remove("active");
+});
+
+});
+
+
