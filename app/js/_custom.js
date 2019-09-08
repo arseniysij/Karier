3b298f5f03bd51a5bc34488bc8517a8e3345c0c0
arseniysij <50396694+arseniysij@users.noreply.github.com>
2019-09-09 01:03:04 +0500

    map lazyload

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -23,7 +23,15 @@ modalClose.addEventListener("click", function(e){
     }
   });
 
-
+var we = $('.why');
+var weTop = we.offset().top;
+$(window).bind('scroll', function(){
+	var windowTop = $(this).scrollTop();
+	if (windowTop > weTop) {
+		$('.map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7c28cf1ba0b6401a3577a6d6c8835f204428399fae6f6cdf5b9c99d7f1c94f03&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true"></script>');
+		$(this.window).unbind('scroll');
+	}
+});
 
 });
 
